543b4fa143c06b833eb03bdcb8a6d72133057d33
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-07-29 23:31:42 +0530

    Implement user register and login APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Memora API is running...');
 });
+
+app.use('/api/auth', authRoutes);
 
 export default app;
